68b3ac8cdd9d131ff3f8b642104f1907491e07c8
Noel <36836739+NoelShin@users.noreply.github.com>
2019-10-07 20:06:23 +0900

    Update bolzman.py
    Add epsilon value (i.e., 1e-8) to avoid division with 0 value.

--- bolzman.py
+++ bolzman.py
@@ -1,26 +1,20 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-np.seterr(divide='ignore', invalid='ignore')
-
-#numpy.seterr(all=None, divide=None, over=None, under=None, invalid=None)[source]
-#Set how floating-point errors are handled.
-#Note that operations on integer scalar types (such as int16) are handled like floating point, and are affected by these settings.
 
 def boltzman(T):
-
     k = 8.617e-5
     E1 = -13.6
     E2 = -3.4
-    N1 = 2 * (np.exp(-E1 / k*T))
-    N2 = 8 * (np.exp(-E2 / k*T))ㅊ
-    number = N2/(N1+N2)
+    N1 = 2 * (np.exp(-E1 / (k * T + 1e-8)))
+    N2 = 8 * (np.exp(-E2 / (k * T + 1e-8)))
+    number = N2 / (N1 + N2 + 1e-8)
     return number
 
-T = np.arange(5000,25000)
+
+T = np.arange(5000, 25000)
 
 number = boltzman(T)
 
-plt.plot(T,number)
-
+plt.plot(T, number)
 plt.show()
